fb1595783e680682855958406d633e5d79656813
KaiserOps <patils533333@gmail.com>
2025-06-13 15:36:19 +0530

    add popup function for body parts

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function selectGender(gender) {
   const image = document.getElementById('body-image');
-  const container = document.getElementById('body-container');
+  const bodyContainer = document.getElementById('body-container');
+  const genderSelect = document.getElementById('gender-select');
 
   if (gender === 'male') {
-    image.src = 'https://i.postimg.cc/c4SPF0Gj/IMG-20250613-WA0005.jpg';  // male image
+    image.src = 'https://i.postimg.cc/c4SPF0Gj/IMG-20250613-WA0005.jpg';
   } else {
-    image.src = 'https://i.postimg.cc/hjBgqx78/24469993-realistic-front-muscles-of-woman-isolated-in-white-background.jpg'; //female image 
+    image.src = 'https://i.postimg.cc/hjBgqx78/24469993-realistic-front-muscles-of-woman-isolated-in-white-background.jpg'; // replace if needed
   }
 
-  container.style.display = 'block';
+  bodyContainer.style.display = 'block';
+  genderSelect.style.display = 'none';
+}
+
+function zoneClicked(part) {
+  const output = document.getElementById('output');
+  output.innerHTML =
+  
+ ` <strong>${part} selected</strong> <br/> 
+  Loading symptoms...`
+
+  ;
 }
